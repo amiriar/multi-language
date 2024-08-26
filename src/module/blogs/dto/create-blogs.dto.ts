931bdd33561c5657af22df0702ed56f99f7caa4f
A.Abdolrahimi <amiriar.irr@gmail.com>
2024-08-26 09:31:24 +0330

    create and update dto added

--- src/module/blogs/dto/create-blogs.dto.ts
+++ src/module/blogs/dto/create-blogs.dto.ts
@@ -1,2 +1,42 @@
+import { IsString, IsNotEmpty, IsOptional, IsArray, IsNumber, IsMongoId, ArrayNotEmpty, MaxLength } from 'class-validator';
+
 export class CreateBlogDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(500)
+  description?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  body: string;  // Assuming the body is in string format (e.g., HTML or Markdown)
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsNumber()
+  timeToRead?: number;
+
+  @IsOptional()
+  @IsString()
+  shortLink?: string;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  authorId: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsMongoId({ each: true })
+  relatedPosts?: string[];
 }
--- src/module/blogs/dto/update-blogs.dto.ts
+++ src/module/blogs/dto/update-blogs.dto.ts
@@ -1,3 +1,9 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsMongoId, IsOptional } from 'class-validator';
+import { CreateBlogDto } from './create-blogs.dto';
 
-export class UpdateBlogDto {
+export class UpdateBlogDto extends PartialType(CreateBlogDto) {
+  @IsOptional()
+  @IsMongoId()
+  id?: string; 
 }
